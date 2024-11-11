fc7b91dc5460000366fbf8d99ef8ba177c8080e9
MIM-104 <134318051+MIM-104@users.noreply.github.com>
2024-11-11 19:13:37 +0000

    customlib.lua

--- customlib.lua
+++ customlib.lua
@@ -11,7 +11,7 @@ speakingheademoji | Fixing + Custom Theme
 
 
 
-local Release = "Build 1.2"
+local Release = "Build 1.21"
 local RayfieldFolder = "Rayfield"
 local ConfigurationFolder = RayfieldFolder.."/Configurations"
 local ConfigurationExtension = ".rfld"
